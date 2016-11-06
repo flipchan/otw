b142b763ca825bd04dbd6b207a68665549cdbe29
root <root@blackangel.flipchan.se>
2016-11-06 19:38:01 +0000

    stable version

--- setup.py
+++ setup.py
@@ -1,77 +1,31 @@
-"""off the wire crypto"""
+import os
+from setuptools import setup
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='otw',
-    version='1.2',
-
-    description='Off the wire crypto',
-    long_description=long_description,
-
-    url='https://github.com/flipchan/otw',
-
-    author='Filip Kalebo',
-    author_email='flipchan@riseup.net',
-
-    license='beerware',
-
+    name = "off the wire",
+    version = "1.3",
+    author = "Filip Kalebo",
+    author_email = "flipchan@riseup.net",
+    description = ("Off the wire crypto"),
+    license = "Beerware",
+    keywords = "encryption layerprox aes aes-ctr pgp hmac secure",
+    url = "https://github.com/flipchan/otw",
+    packages=['otw'],
+    install_requires=['gnupg'],
+    long_description=read('README'),
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stabl
-        'Development Status :: 5 - Stable',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'License :: OSI Approved :: BSD license',
-
-      
+        "Development Status :: 5 - Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
-
-    # What does your project relate to?
-    keywords='encryption layerprox aes aes-ctr pgp hmac',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    packages=['otw', 'source']
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    install_requires=['gnupg'],
-
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        'sample': ['package_data.dat'],
-    },
-
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
 )
